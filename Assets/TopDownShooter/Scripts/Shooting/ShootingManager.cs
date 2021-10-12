d780cef9ef9c41a4f4e9de32292f4289d6919b13
FatihCETİNER <fatihcetiner74@gmail.com>
2021-10-12 16:29:04 +0300

    Ground Assets

---
+++ Assets/TopDownShooter/Scripts/Shooting/ShootingManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.Shooting
+{
+    public class ShootingManager : MonoBehaviour
+    {
+       
+        public void Shoot(Vector3 from,Vector3 direction)
+        {
+            RaycastHit hit;
+
+           bool rayhit= Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity);
+
+
+
+        }
+
+    }
+
+
+}
